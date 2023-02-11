7f75f82f2c7936e020c0fbcc39e38abf1667aa09
Othman Manneta <othman.manneta@gmail.com>
2023-02-11 11:53:21 +0100

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="App"> Manneta
+    <div className="App">
+      <div className="container">
+          <Navbar />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
