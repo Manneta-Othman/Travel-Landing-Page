a243f1f6a6d1863c488dad5cd02e3948161d656e
Othman Manneta <othman.manneta@gmail.com>
2023-02-11 13:27:23 +0100

    navbar done and start working on background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.scss';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-          <Navbar />
+        <Navbar/>
       </div>
     </div>
   );
